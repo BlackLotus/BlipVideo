748c71ae2762653e00a3d85433fd5769b82588b1
Thomas Schneider <maxmusterm@gmail.com>
2015-02-15 03:44:40 +0100

    basic functions integrated

--- blip.py
+++ blip.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-import urllib,re,sys
+import urllib,re,sys,shlex
 
 class BlipVideo:
+
     def __init__(self,url=""):
         if re.match('.*blip.tv/.*',url):
-            pass
+            rss=self.get_rss(url)
+            self.get_info(rss)
 
     def get_info(self,url):
-        pass
+        site=urllib.urlopen(url).read()
+        title=re.findall('<media:title>(.+)</media:title>',site)[0]
+        self.blips=dict(re.findall('<blip:(.+)>(.+)</blip:.+>',site))
+        self.medias=[]
+        for media in re.findall('<media:content (.+)\/>',site):
+            self.medias.append(dict([re.findall('(.*)=(.*)',m)[0] for m in shlex.split(media)]))
 
     def get_rss(self,url):
         urlR=re.compile('http:\/\/blip\.tv/([^/]+)/([^/]+)-(\d+)')
         (channel,name,vid)=urlR.findall(url)[0]
         return 'http://blip.tv/rss/flash/%s' % vid
 
-    def check(self):
-        pass
-
-
 if __name__=="__main__":
-    blip=BlipVideo()
+    blip=BlipVideo('http://blip.tv/the-spoony-experiment/final-fantasy-xiii-review-part-4-6588174')
+    #print blip.blips
+#    print blip.get_rss('http://blip.tv/the-spoony-experiment/final-fantasy-xiii-review-part-4-6588174')
